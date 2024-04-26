de5dd3897d953a6fec064d61585e922f85fe2c3d
ChandraShekhar <csyadav0513@gmail.com>
2024-04-26 15:30:23 +0530

    Add API route for sending messages

--- src/model/User.ts
+++ src/model/User.ts
@@ -2,27 +2,20 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface Message extends Document {
   content: string;
-  createdAT: Date;
-  updatedAT: Date;
+  createdAt: Date;
 }
 
-const MessageSchema: Schema<Message> = new Schema({
+const MessageSchema: Schema<Message> = new mongoose.Schema({
   content: {
     type: String,
     required: true,
   },
-  createdAT: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
-  updatedAT: {
+  createdAt: {
     type: Date,
     required: true,
     default: Date.now,
   },
 });
-
 
 export interface User extends Document {
   username: string;
@@ -35,31 +28,31 @@ export interface User extends Document {
   messages: Message[];
 }
 
-const UserSchema: Schema<User> = new Schema({
+// Updated User schema
+const UserSchema: Schema<User> = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, "Username is required 😁"],
+    required: [true, "Username is required"],
     trim: true,
     unique: true,
   },
   email: {
     type: String,
-    required: [true, "Email is required 😋"],
-    trim: true,
+    required: [true, "Email is required"],
     unique: true,
-    match: [/.+\@.+\..+/, "Please fill a valid email address 🫠"],
+    match: [/.+\@.+\..+/, "Please use a valid email address"],
   },
   password: {
     type: String,
-    required: [true, "Password is required 😅"],
+    required: [true, "Password is required"],
   },
   verifyCode: {
     type: String,
-    required: [true, "Verification code is required 🙂"],
+    required: [true, "Verify Code is required"],
   },
   verifyCodeExpiry: {
     type: Date,
-    required: [true, "Verification code expiry is required 🫦"],
+    required: [true, "Verify Code Expiry is required"],
   },
   isVerified: {
     type: Boolean,
